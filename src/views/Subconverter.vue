38ba6de2255c008456f256a1f4fc152fa8a34772
翠花 <admin@nange.cn>
2020-12-12 20:27:22 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -381,7 +381,7 @@ export default {
     };
   },
   created() {
-    document.title = "Subscription Converter";
+    document.title = "订阅转换";
     this.isPC = this.$getOS().isPc;
 
     // 获取 url cache
