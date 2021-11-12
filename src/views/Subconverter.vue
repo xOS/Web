dfa98d4a94507ed88a460592f4ff5de20c91f9e6
翠花 <admin@nange.cn>
2021-11-13 01:21:22 +0800

    Update.

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -354,7 +354,6 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash";
-    //this.notify();
     this.getBackendVersion();
   },
   methods: {
@@ -509,19 +508,6 @@ export default {
         .finally(() => {
           this.loading = false;
         });
-    },
-    notify() {
-      const h = this.$createElement;
-
-      this.$notify({
-        title: "隐私提示",
-        type: "warning",
-        message: h(
-          "i",
-          { style: "color: teal" },
-          "各种订阅链接（短链接服务除外）生成纯前端实现，无隐私问题。默认提供后端转换服务，隐私担忧者请自行搭建后端服务。"
-        )
-      });
     },
     confirmUploadConfig() {
       if (this.uploadConfig === "") {
