d21e789503b71d10bf2cde7c96fbdfb85dab0cbf
xOS <admin@nange.cn>
2021-11-14 17:40:16 +0000

    Deploying to gh-pages from  @ 95ed8dca399d4b60eb50a140ce4ebcd4fcd14fda 🚀

---
+++ js/precache-manifest.85fd665adb3b67618329e98ce0915567.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "1ef14ad3bbe97b311ef1",
+    "url": "/css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "1f42c09faae425902cb9",
+    "url": "/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "48a904d4346bca21a384bb07fa388746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef14ad3bbe97b311ef1",
+    "url": "/js/app.58cac622.js"
+  },
+  {
+    "revision": "15db7b1934346e0eb934",
+    "url": "/js/chunk-4e30bce1.53d0e77f.js"
+  },
+  {
+    "revision": "1f42c09faae425902cb9",
+    "url": "/js/chunk-vendors.3bfb6c45.js"
+  },
+  {
+    "revision": "613700203099ec62561506293cdd53c2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
